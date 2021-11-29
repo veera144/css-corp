96d4d457694c7c20eb67368660b80bc6eed7acd6
Veerasekaran <veera144@gmail.com>
2021-11-29 22:28:47 +0530

    Veerasekaran Day 1 tasks

--- Day1.js
+++ Day1.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+const a = 1; const b = 2; const c = 3;
+
+(function firstFunction () {
+  const b = 5; const c = 6;
+
+  (function secondFunction () {
+    const b = 8;
+    console.log(`a: ${a}, b: ${b}, c: ${c}`);
+
+    (function thirdFunction () {
+      const a = 7; const c = 9;
+
+      (function fourthFunction () {
+        const a = 1; const c = 8;
+      })()
+    })()
+  })()
+})()
+
+// Can be solved by passing the bird param to IIFE or declaring the variable inside the IIFE. 
+var bird = 'Pidgeons';
+( function (bird) {
+    //var bird = 'Pidgeons';
+    if ( typeof bird === 'undefined' ){
+        var bird = 'Rubber Duck';
+        console.log('Ernie loves his ' + bird );
+    } else {
+        console.log('Bert loves his ' + bird );
+    }
+}(bird) );
