19ba7bfa8e1b14dcdb1596eae3ebc390f9d54a7f
Veerasekaran <veera144@gmail.com>
2021-12-05 22:32:47 +0530

    Day 3 task

---
+++ Day3.js
@@ -0,0 +1,16 @@
+const users = [ { id: 1, name: 'Yagnesh', gender: 'Male', age: 33},
+                 { id: 2, name: 'Virat', gender: 'Male', age: 30}, 
+                 { id: 3, name: 'Rohit', gender: 'Male', age: 28}, 
+                 { id: 7, name: 'Taimur', gender: 'Male', age: 08}, 
+                 { id: 4, name: 'Alia', gender: 'Female', age: 18}, 
+                 { id: 5, name: 'Dipeeka', gender: 'Female', age: 24}, 
+                 { id: 6, name: 'Priyanka', gender: 'Female', age: 38}, ];
+
+const groupByAge = users.reduce((p, c) => { 
+    const ageGroup = Math.floor(c.age/10);
+    const key = `${ageGroup}0-${ageGroup}9`;
+    (p[key] = p[key] || []).push(c); 
+    p[key].sort((firstItem,secondItem) => firstItem.name === secondItem.name ? 0 : firstItem.name < secondItem.name ? -1 : 1)   
+    return p;
+}, {})
+console.log(groupByAge);
